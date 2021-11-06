56905e7612e91662313234f2dfd4edaa6e4663e8
Gilad Penn <freeslugs@gmail.com>
2021-11-06 15:34:19 -0500

    connect to magiclink

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -2,11 +2,14 @@ import Web3 from "web3";
 // import ComplexStorage from "./contracts/ComplexStorage.json";
 import SimpleStorage from "./contracts/SimpleStorage.json";
 import TutorialToken from "./contracts/TutorialToken.json";
+import { Magic } from 'magic-sdk';
+
+const magic = new Magic('pk_live_925E22A1B237DBBB', { network: 'rinkeby' });
 
 const options = {
   web3: {
-    block: false,
-    // customProvider: new Web3("ws://localhost:8545"),
+    // block: false,
+    customProvider: new Web3(magic.rpcProvider)
   },
   contracts: [SimpleStorage, TutorialToken],
   events: {
