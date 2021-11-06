faf8c79bdeb1b5bfc1797ec20ded0b4a5a999933
Trace Thompson <tracethmpsn@gmail.com>
2021-11-06 15:09:56 -0500

    mobile

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -20,20 +20,24 @@ export default ({ drizzle, drizzleState, initialized }) => {
     // fetchData();
   });
 
+  const handleTopUp = () => {
+    console.log('top up')
+  }
+
   return (
-    <div className="w-3/5 mx-auto bg-light-main rounded p-4">
+    <div className="w-full md:w-3/5 mx-auto bg-light-main rounded p-2 md:p-4">
       {/* { !initialized ? 
         <img src="./brrrrr.png"/>
       : */}
       <>
         <h1 className="font-bold text-3xl text-light-green">brrrrr Finance</h1>
-        <div className="flex mt-4 items-center flex-wrap">
+        <div className="flex mt-4 items-center w-full flex-wrap">
           <h2 className="text-white text-2xl font-bold w-1/2 lg:w-1/3">Balance</h2>
-          <h2 className="text-white text-2xl text-white w-1/2 lg:w-1/3">{usdcBal} USDC</h2>
-          <button className="rounded px-2 py-1 bg-white text-black cursor-pointer lg:w-1/3">Top Up</button>
+          <h2 className="text-white text-2xl text-white w-1/2 ml-auto lg:ml-0 text-right lg:text-left lg:w-1/3">{usdcBal} USDC</h2>
+          <button className="rounded px-2 py-1 bg-white text-black cursor-pointer w-full lg:w-1/3" onClick={handleTopUp}>Top Up</button>
         </div>
 
-        <TokenList />
+        <TokenList setUsdcBal={setUsdcBal}/>
       </>
     </div>
   );
--- app/src/components/TokenList.js
+++ app/src/components/TokenList.js
@@ -13,28 +13,51 @@ export default ({ drizzle, drizzleState, initialized }) => {
     setTokens(tokensMock)
   }, []);
 
+  const handleApe = token => {
+    console.log('LOOK AT THAT GOD CANDLE ', token.contract)
+  }
+
+  const handleRug = token => {
+    console.log('YEEOAAAAAAAAOWWWW IT WENT TO ZERO ', token.contract)
+  }
+
+  const gigaApe = () => {
+    console.log('GIGA APE')
+  }
+
+  const paperHand = () => {
+    console.log('paper hand bitch')
+  }
+
   return (
     <div className="w-full mt-6">
-      <h1 className="">Shitcoins</h1>
+      <button className="rounded px-2 py-1 bg-white text-black cursor-pointer w-full lg:w-1/3 mt-2 bg-light-green text-white" onClick={gigaApe}>Giga Ape</button>
       { tokens && tokens.map(token => {
         return (
           <div className="rounded bg-gray mt-2 flex-col p-2">
             <div className="text-xl flex text-white">
               <h1 className="mr-4 font-bold">{token.name}</h1>
-              <h1 className="text-light-green">{token.price}</h1>
-              <a className="ml-auto text-sm underline cursor-pointer" href={`https://www.dextools.io/app/ether/pair-explorer/${token.pair}`}>Paint the Chart</a> 
+              <h1 className="text-light-green ml-auto md:ml-0">{token.price}</h1>
+              <a className="ml-auto text-sm underline cursor-pointer hidden md:block" href={`https://www.dextools.io/app/ether/pair-explorer/${token.pair}`}>Chart</a> 
             </div>
-            <div className="flex">
+            <div className="flex items-center mt-2">
               <h1 className="mr-2 font-bold text-grey-560">Balance</h1>
               <h1 className="">{token.bal}</h1>
+
+              <div className="ml-auto hidden md:flex">
+                <button className="bg-white text-black mr-2 py-1 px-2 rounded" onClick={() => handleApe(token)}>Ape</button>
+                <button className="bg-white text-black py-1 px-2 rounded" onClick={() => handleRug(token)}>Rug</button>
+              </div>
             </div>
-            <div>
-              <button></button>
-              <button></button>
+            <div className="w-full flex md:hidden items-center">
+            <a className="text-sm text-white underline cursor-pointer block md:hidden" href={`https://www.dextools.io/app/ether/pair-explorer/${token.pair}`}>Chart</a> 
+              <button className="bg-white text-black mr-2 py-1 px-2 rounded ml-auto" onClick={() => handleApe(token)}>Ape</button>
+              <button className="bg-white text-black py-1 px-2 rounded ml-2" onClick={() => handleRug(token)}>Rug</button>
             </div>
           </div>
         )
         })}
+        <button className="rounded px-2 py-1 bg-white text-white cursor-pointer w-full lg:w-1/3 mt-2 bg-red" onClick={paperHand}>Paper Hand</button>
     </div>
   );
 };
